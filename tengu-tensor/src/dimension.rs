9bfcf02f701d3785faeff0e8ec5405ea2c88caa0
Valerii Praid <valerii.praid@gmail.com>
2022-04-30 15:53:56 +0000

    Fix dense values iteration and add 'at' method

--- tengu-tensor/src/dimension.rs
+++ tengu-tensor/src/dimension.rs
@@ -30,7 +30,21 @@ impl Dimension {
     /// in the chain.
     pub fn values<'a, T>(&self, start: usize, data: &'a [T]) -> impl Iterator<Item = &'a T> {
         match self {
-            Dimension::Dense { size } => data[start..start + *size].iter()
+            Dimension::Dense { size } => {
+                let offset = start * size;
+                data[offset..offset+size].iter()
+            }
+        }
+    }
+
+    /// Returns the position of the coordinate in this dimension if it is nonzero, None otherwise.
+    /// This operation essentially flattens the current dimension. For example, consider a 2 by 3
+    /// matrix with both dimensions dense. If we need to get the position of the coordinate (1, 2),
+    /// we need 3 elements from row 0 and 2 from row 1 to get the coordinate position, which will
+    /// be 5.
+    pub fn at(&self, start: usize, index: usize) -> Option<usize> {
+        match self {
+            Dimension::Dense { size } => Some(start * size + index)
         }
     }
 }
@@ -41,11 +55,11 @@ mod tests {
 
     #[test]
     fn test_dense_dimension() {
-        let dim = Dimension::Dense { size: 10 };
-        assert_eq!(dim.size(), 10);
-        assert_eq!(dim.positions().collect::<Vec<_>>(), (0..10).collect::<Vec<_>>());
-        assert_eq!(dim.indices().collect::<Vec<_>>(), (0..10).collect::<Vec<_>>());
-        assert_eq!(dim.values(0, &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).copied().collect::<Vec<_>>(),
-                   (0..10).collect::<Vec<_>>());
+        let dim = Dimension::Dense { size: 3 };
+        assert_eq!(dim.size(), 3);
+        assert_eq!(dim.positions().collect::<Vec<_>>(), (0..3).collect::<Vec<_>>());
+        assert_eq!(dim.indices().collect::<Vec<_>>(), (0..3).collect::<Vec<_>>());
+        assert_eq!(dim.values(1, &[0, 1, 2, 3, 4, 5]).copied().collect::<Vec<_>>(), vec![3, 4, 5]);
+        assert_eq!(dim.at(1, 2), Some(5));
     }
 }
